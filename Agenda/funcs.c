d60b4f7f42bdb7e83f986ef1756644a81583a1e6
VlNlClUS <56849654+VlNlClUS@users.noreply.github.com>
2020-04-26 14:56:11 -0300

    Add files via upload

--- Agenda/funcs.c
+++ Agenda/funcs.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include "funcs.h"
+#define DIM 30
 
-int teste(int x)
+
+
+
+
+
+void inicalizarLLS(LISTA *LLS)
 {
+    LLS->nmrElem = 0;            //Função serve para inicializar a lista, zero elementos validos, está recebendo o endereço da lista para que as modificações sejam realizadas no campo certo, caso contrario seria modifica a copia da estrutura
+}
 
-    printf("RESPOSTA = %d",x);
+
+
+int inserirElemPosicaoLLS_Ord(LISTA *LLS, REGISTRO valor)
+{
+    if (LLS->nmrElem >= DIM)
+        return 0;
+
+    int i, pos;
+
+    for (pos = LLS->nmrElem; (pos > 0) && (LLS->A[pos - 1].teste > valor.busqData); pos--)
+    {
+        LLS->A[pos] = LLS->A[pos - 1];
+    }
+    LLS->A[pos] = valor;
+    LLS->nmrElem++;
+    return 1;
+}
+
+void mostraLLS(LISTA *LLS)
+{
+    int i;
+    printf("LISTA: < ");
+    for (i = 0; i < LLS->nmrElem; i++)
+    {
+        printf("%i, ", LLS->A[i].busqData);
+    
+    }
+    fflush(stdin); //fflush(stdin); //se for windows
+    printf(" >\n Digite <enter> para voltar");
+    getchar();
 }
--- Agenda/main.c
+++ Agenda/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "funcs.h"
+#define DIM 30
+
+
+
+
+
 int main(){
 
 
-    int x = 0;
+    int x = 0,TEMP;
    char opcao[20];
-  
+   LISTA LLS;
+   REGISTRO valor;
+    inicalizarLLS(&LLS);
 
    
     do
@@ -27,16 +36,56 @@ int main(){
               else if (opcao[0] == 'c') opcao[0]='C';
                else if (opcao[0] == 'e') opcao[0]='E';
                 else if (opcao[0] == 'f') opcao[0]='F';
-                
-                 getchar();  
+                 
+              fflush(stdin);
+              getchar();
         switch (opcao[0])
         
         
         {
         
         case 'I':
-         printf("TESTE I");
-         
+        
+          printf(" Preencha os campos para que a tarefa seja computada:\n\n");
+          printf("Digite o Nome do Trabalho");
+							fflush(stdin);
+							getchar();
+							fgets(valor.nometrab, 20, stdin);
+          printf("Digite uma Descricao para o trabalho");
+							fflush(stdin);
+							getchar();
+							fgets(valor.desc, 80, stdin);
+          
+          printf("Digite a Data de Entraga para do trabalho:\n");
+          printf("Dia: ");
+          scanf("%d", &valor.dd);
+           printf("Mes: ");
+          scanf("%d", &valor.mm);
+           printf("Ano: ");
+          scanf("%d", &valor.ano);
+            valor.busqData=(valor.ano*10000)+(valor.mm*100)+valor.dd;
+
+            TEMP = inserirElemPosicaoLLS_Ord(&LLS, valor);
+            if (TEMP == 0)
+            {
+                printf("Ocorreu um ERRO, talvez a posicao desejada nao exista na lista\n"); //Ou está vazia na lista
+            }
+            else
+            {
+                printf("SUCESSO\n");
+            }
+        
+            printf("Digite <enter> para voltar");
+            getchar();
+
+
+
+
+
+
+
+
+
             break;
 
         case 'R':
@@ -44,8 +93,9 @@ int main(){
        
             break;
         case 'C':
-        printf("TESTE C");
+        mostraLLS(&LLS);
      
+         
             break;
         case 'E':
           printf("TESTE E");
